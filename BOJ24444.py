7a3dfb9623f127f8aa8a9659ecad46b31dc7bac2
HSJ <sjhan1@naver.com>
2025-04-02 05:40:56 +0900

    add 0401

--- BOJ24444.py
+++ BOJ24444.py
@@ -1,8 +1,32 @@
-# 25.03.31
+# 25.04.01
+
+from collections import deque
 
 N, M, R = map(int, input().split())
-arr = [[0] * (N+1) for _ in range(N+1)]
-for i in range(M):
+arr = [[] for _ in range(N+1)]          # 2차원 배열 메모리 초과 >>> arr list 로 변경
+for _ in range(M):
     u, v = map(int, input().split())
-    arr[u][v] = arr[v][u] = 1
-    
+    arr[u].append(v)
+    arr[v].append(u)
+visited = [0] * (N+1)
+q = deque()
+
+
+def bfs(r):
+    visited[r] = 1
+    q.append(r)
+    cnt = 2
+    while q:
+        k = q.popleft()
+        arr[k].sort()
+        for i in arr[k]:
+            if visited[i] > 0:
+                continue
+            visited[i] = cnt
+            cnt += 1
+            q.append(i)
+
+
+bfs(R)
+for i in range(1, N+1):
+    print(visited[i])
